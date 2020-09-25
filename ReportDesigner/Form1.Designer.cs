c45d4c8fe2469b77367950a63b7fd2b59caa2082
James Fu <jamesfu_thu@msn.com>
2020-09-25 23:36:55 +0800

    空殼

--- ReportDesigner/Form1.Designer.cs
+++ ReportDesigner/Form1.Designer.cs
@@ -29,12 +29,117 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.lblServer = new System.Windows.Forms.Label();
+            this.txtServer = new System.Windows.Forms.TextBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnDesign = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblServer
+            // 
+            this.lblServer.Location = new System.Drawing.Point(13, 15);
+            this.lblServer.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblServer.Name = "lblServer";
+            this.lblServer.Size = new System.Drawing.Size(144, 25);
+            this.lblServer.TabIndex = 0;
+            this.lblServer.Text = "SQL Server";
+            this.lblServer.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // txtServer
+            // 
+            this.txtServer.Location = new System.Drawing.Point(164, 12);
+            this.txtServer.Name = "txtServer";
+            this.txtServer.ReadOnly = true;
+            this.txtServer.Size = new System.Drawing.Size(304, 34);
+            this.txtServer.TabIndex = 1;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(164, 69);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(679, 195);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(13, 72);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "SQL";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(164, 289);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(91, 91);
+            this.btnTest.TabIndex = 4;
+            this.btnTest.Text = "Test";
+            this.btnTest.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(261, 289);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(95, 91);
+            this.btnSave.TabIndex = 5;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnDesign
+            // 
+            this.btnDesign.Location = new System.Drawing.Point(362, 289);
+            this.btnDesign.Name = "btnDesign";
+            this.btnDesign.Size = new System.Drawing.Size(90, 91);
+            this.btnDesign.TabIndex = 6;
+            this.btnDesign.Text = "Design";
+            this.btnDesign.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(855, 392);
+            this.Controls.Add(this.btnDesign);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnTest);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtServer);
+            this.Controls.Add(this.lblServer);
+            this.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblServer;
+        private System.Windows.Forms.TextBox txtServer;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnDesign;
     }
 }
 
